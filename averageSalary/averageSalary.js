3e65d36402011ac7f8db801c8ef078ec0b12ca60
Jordan Villacorta <jordanvillacorta@safetycuresmbp2.home>
2021-05-18 14:22:05 -0400

    added revised solution to averageSalary taa exercise

--- averageSalary/averageSalary.js
+++ averageSalary/averageSalary.js
@@ -1,10 +1,19 @@
-function (salary) {
+  function(salary) {
     let avgSalary = 0;
     let newSalary = salary.sort();
-    newSalary.pop();
-    newSalary.shift();
+    // newSalary.pop();
+    // newSalary.shift();
+    let minSal = newSalary[0];
+    let maxSal = newSalary[0];
     for (let i = 0; i < newSalary.length; i++) {
+      if (newSalary[i] < minSal) {
+        minSal = newSalary[i];
+      }
+      if (newSalary[i] > maxSal) {
+        maxSal = newSalary[i];
+      }
       avgSalary += newSalary[i];
     }
-    return avgSalary / newSalary.length;
+    
+    return (avgSalary - minSal - maxSal) / (newSalary.length - 2);
   };
