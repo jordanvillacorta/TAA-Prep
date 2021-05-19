1e35f94c0b1625353d6f8e2e5f825bbc5a1ddd27
jordanvillacorta <jordan.villacorta@gmail.com>
2021-05-18 22:42:54 -0400

    added solutions and tests to firstUniqChar taa exercise

--- firstUniqChar/firstUniqChar.js
+++ firstUniqChar/firstUniqChar.js
@@ -1,3 +1,38 @@
-var firstUniqChar = function(s) {
-    
+Solution #1:
+let firstUniqChar = function(s) {
+  let splitString = s.split("");
+  let count = 0;
+
+  for(let i = 0; i < splitString.length; i++) {
+    count = 0;
+    let currentLetter = splitString[i];
+
+    for(let j = 0; j < splitString.length; j++) {
+      if (currentLetter === splitString[j]) {
+        count += 1;
+      }    
+    }
+
+    if(count === 1) {
+      return i;
+    }
+  }
+  
+  return -1;
 };
+
+
+Solution #2: 
+let firstUniqChar = function(string) {
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+    if (string.indexOf(char) == i && string.indexOf(char, i + 1) == -1) {
+    return i;
+  }
+}
+
+return -1;
+};
+
+
+
