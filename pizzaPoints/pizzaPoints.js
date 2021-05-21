1b5864cb312c02628441cabeed5ac11f58a7b7ad
jordanvillacorta <jordan.villacorta@gmail.com>
2021-05-20 22:16:22 -0400

    added revised solution to ransomNote taa exercise

---
+++ pizzaPoints/pizzaPoints.js
@@ -0,0 +1,3 @@
+function pizzaPoints(customers, minOrders, minPrice) {
+	
+}
--- ransomNote/ransomNote.js
+++ ransomNote/ransomNote.js
@@ -1,12 +1,10 @@
 var canConstruct = function(ransomNote, magazine) {
-  let cutOutMagazine = magazine.split('')
+  let magazineLetters = magazine.split('')
   for (let i = 0; i < ransomNote.length; i++) {
-    let currentLetter = ransomNote[i]
-    let currentLetterIndex = cutOutMagazine.indexOf(currentLetter)
-    if (!cutOutMagazine.includes(currentLetter)) {
+    if (!magazineLetters.includes(ransomNote[i])) {
       return false
     }
-    cutOutMagazine.splice(currentLetterIndex, 1, '')
+    magazineLetters.splice(magazineLetters.indexOf(ransomNote[i]), 1, '')
   }
   return true
 }
