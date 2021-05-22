9e92cbe2dec7709dee166e94ef2b9a62c519c728
Jordan Villacorta <83678670+jordanvillacorta@users.noreply.github.com>
2021-05-22 17:32:50 -0400

    Update pizzaPoints.js

--- pizzaPoints/pizzaPoints.js
+++ pizzaPoints/pizzaPoints.js
@@ -3,9 +3,9 @@ function pizzaPoints(customers, minOrders, minPrice) {
 	for (let name in customers) {
 		let qualifiedOrders = []
 		for (let index = 0; index < customers[name].length; index++) {
-		  if (customers[name][index] >= minPrice) {
-				qualifiedOrders.push(customers[name][index])
-			}
+				if (customers[name][index] >= minPrice) {
+					qualifiedOrders.push(customers[name][index])
+				}
 	  }
 		if (qualifiedOrders.length >= minOrders) {
 			freePizzaWinners.push(name)
