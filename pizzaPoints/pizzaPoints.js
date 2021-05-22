76176aa7411d3123bee03cb82ea918a29549b21d
jordanvillacorta <jordan.villacorta@gmail.com>
2021-05-22 17:28:10 -0400

    added solution to pizzaPoints taa exercise

--- pizzaPoints/pizzaPoints.js
+++ pizzaPoints/pizzaPoints.js
@@ -1,3 +1,16 @@
 function pizzaPoints(customers, minOrders, minPrice) {
+	let freePizzaWinners = []
+	for (let name in customers) {
+		let qualifiedOrders = []
+		for (let index = 0; index < customers[name].length; index++) {
+		  if (customers[name][index] >= minPrice) {
+				qualifiedOrders.push(customers[name][index])
+			}
+	  }
+		if (qualifiedOrders.length >= minOrders) {
+			freePizzaWinners.push(name)
+		}
+	}
 	
+	return freePizzaWinners.sort()
 }
