4b93a13eecad753e368dabda980138738e746658
jordanvillacorta <jordan.villacorta@gmail.com>
2021-05-24 13:51:29 -0400

    added solution to whoIsTheWinner taa exercise

--- whoIsTheWinner/whoIsTheWinner.js
+++ whoIsTheWinner/whoIsTheWinner.js
@@ -1,3 +1,18 @@
 function determineWinnerOfFight(obj) {
-	
+	let meCounter = 0;
+	let spouseCounter = 0;
+	for (let key in obj) {
+		let round = obj[key]
+		if (obj[key].me > obj[key].spouse) {
+			meCounter++
+		} else if (obj[key].spouse > obj[key].me) {
+			spouseCounter++
+		}
+	}
+	if (meCounter > spouseCounter) {
+		return 'ME!'
+	} else if (spouseCounter > meCounter) {
+		return 'SPOUSE!'
+	}
+	return 'NOBODY!'
 }
