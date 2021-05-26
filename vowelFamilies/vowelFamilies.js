ad25340fd3e52ec9501a1fe8c3fb6e2a789a979b
jordanvillacorta <jordan.villacorta@gmail.com>
2021-05-25 23:16:32 -0400

    added revised solution to vowelFamilies taa exercise

--- vowelFamilies/vowelFamilies.js
+++ vowelFamilies/vowelFamilies.js
@@ -1,34 +1,34 @@
 function sameVowelGroup(wordList) {
-	let sameVowelList = []
 	let vowelsInFirstWord = vowelFinder(wordList[0])
+	let resultVowelList = []
 	for (let i = 0; i < wordList.length; i++) {
 		let vowelsInCurrentWord = vowelFinder(wordList[i])
-		let vowelCount = 0
+		let vowelCounter = 0
 		for (let j = 0; j < vowelsInCurrentWord.length; j++) {
 			if (vowelsInFirstWord.includes(vowelsInCurrentWord[j])) {
-				vowelCount++
+				vowelCounter++
 			}
-			if (vowelCount === vowelsInCurrentWord.length) {
-				sameVowelList.push(wordList[i])
+			if (vowelCounter === vowelsInCurrentWord.length) {
+				resultVowelList.push(wordList[i])
 			}
 		}
 	}
-	return sameVowelList
+	return resultVowelList
 }
 
 function vowelFinder(word) {
 	let vowels = ['a', 'e', 'i', 'o', 'u']
-	let vowelContainer = []
+	let foundVowels = []
 	for (let v = 0; v < word.length; v++) {
 		if (vowels.includes(word[v])) {
-			vowelContainer.push(word[v])
+			foundVowels.push(word[v])
 		}
 	}
 	
 	let uniqueVowels = []
-	for (let x = 0; x < vowelContainer.length; x++) {
-		if (!uniqueVowels.includes(vowelContainer[x])) {
-			uniqueVowels.push(vowelContainer[x])
+	for (let x = 0; x < foundVowels.length; x++) {
+		if (!uniqueVowels.includes(foundVowels[x])) {
+			uniqueVowels.push(foundVowels[x])
 		}
 	}
 	return uniqueVowels
