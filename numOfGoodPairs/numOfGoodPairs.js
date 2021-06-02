ce54153302722c45cd566a1f8bd0564b4d35402a
jordanvillacorta <jordan.villacorta@gmail.com>
2021-06-02 12:43:48 -0400

    added revised solution to numOfGoodPairs taa exercise

--- numOfGoodPairs/numOfGoodPairs.js
+++ numOfGoodPairs/numOfGoodPairs.js
@@ -1,11 +1,11 @@
 var numIdenticalPairs = function(nums) {
-    let index = 0;
-    for (let i = 0; i < nums.length; i++) {
-      for (let j = 1; j < nums.length; j++) {
-        if (nums[i] === nums[j] && i < j) {
-          index++;
-        }
-      };
+  let numberOfGoodPairs = 0
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && i < j) {
+        numberOfGoodPairs++
+      }
     }
-    return index;
-  };
+  }
+   return numberOfGoodPairs
+ };
